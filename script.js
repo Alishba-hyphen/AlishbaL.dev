fbf83e0b4c8984eeded10ca5e36501298f36e4f6
Alishba <alishbasubhan@gmail.com>
2025-06-14 19:41:47 -0400

    Redesigned hero section text and buttons

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
     // VANTA.NET background effect
     VANTA.NET({
-      el: "#hero",
-      mouseControls: true,
-      touchControls: true,
-      gyroControls: false,
-      minHeight: 200.0,
-      minWidth: 200.0,
-      scale: 1.0,
-      scaleMobile: 1.0,
-      color: 0xffffff,
-      backgroundColor: 0x0d0d0d,
-      points: 15.0,
-      maxDistance: 30.0,
-      spacing: 18.0
-    });
+  el: "#hero",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  color: 0xffffff,
+  backgroundColor: 0x0d0d0d,
+  points: 8.0,
+  maxDistance: 20.0,
+  spacing: 25.0
+});
   
     // Typewriter animation
     new Typed("#typed-text", {
